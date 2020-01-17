35fc1d452b6009a7bf00e8e9755248fab9130342
Hassan Saleh <hassan.saleh@ubanquity.com>
2020-01-17 11:17:43 +0200

    Initial commit

---
+++ src/cli.js
@@ -0,0 +1,75 @@
+import arg from 'arg';
+import inquirer from 'inquirer';
+import { generateEntry } from './generate-entry';
+
+const commands = [
+    { value: 'generate', name: 'Generate a new changelog entry' },
+    { value: 'prepare', name: 'Prepare and validate all entries (required after merging release branches together)' },
+    { value: 'create', name: 'Create the changelog file' }
+]
+
+function parseArgumentsIntoOptions(rawArgs) {
+    const args = arg(
+        {
+            // Types
+            '--force': Boolean,
+            '--issue': Number,
+            '--type': String,
+            '--dry-run': Boolean,
+            '--help': Boolean,
+
+            // Aliases
+            '-f': '--force',
+            '-i': '--issue',
+            '-t': '--type',
+            '-d': '--dry-run',
+            '-h': '--help',
+        },
+        {
+            argv: rawArgs.slice(2),
+        }
+    );
+    return {
+        force: args['--force'] || false,
+        issue: args['--issue'],
+        command: args._[0],
+        type: args['--type'],
+        dryRun: args['--dry-run'] || false,
+        help: args['--help'] || false
+    };
+}
+
+async function promptForMissingOptions(options) {
+    const defaultType = 'generate';
+
+    const questions = [];
+
+    if (!options.command || options.command.trim() == '') {
+        questions.push({
+            type: 'list',
+            name: 'command',
+            message: 'Select an action: ',
+            choices: commands,
+            default: defaultType
+        })
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        command: options.command || answers.command
+    };
+}
+
+export async function cli(args) {
+    let options = parseArgumentsIntoOptions(args);
+    options = await promptForMissingOptions(options);
+
+    switch (options.command) {
+        case ('generate'):
+            generateEntry(options)
+            break;
+        default:
+            console.error('Invalid command')
+    }
+}
---
+++ src/generate-entry.js
@@ -0,0 +1,111 @@
+import fs from 'fs'
+import path from 'path'
+import YAML from 'yaml'
+import inquirer from 'inquirer'
+
+const TYPES = [
+    { value: 'added', name: 'New feature' },
+    { value: 'fixed', name: 'Bug fix' },
+    { value: 'changed', name: 'Feature change' },
+    { value: 'deprecated', name: 'New deprecation' },
+    { value: 'removed', name: 'Feature removal' },
+    { value: 'security', name: 'Security fix' },
+    { value: 'performance', name: 'Performance improvement' },
+    { value: 'other', name: 'Other' }
+]
+
+async function promptForMissingOptions(options) {
+    const defaultType = 'changed';
+
+    const questions = [];
+
+    if (!options.title) {
+        questions.push({
+            type: 'input',
+            name: 'title',
+            message: 'Enter a title for the entry: ',
+            validate: (input) => {
+                return new Promise((resolve, reject) => {
+                    if (!input || input.trim() == '') {
+                        reject('You need to provide a title')
+                        return;
+                    }
+                    resolve(true)
+                })
+            }
+        })
+    }
+
+    if (!options.type) {
+        questions.push({
+            type: 'list',
+            name: 'type',
+            message: 'Choose which change type to use: ',
+            choices: TYPES,
+            default: defaultType,
+        });
+    }
+
+    if (!options.issue) {
+        questions.push({
+            type: 'number',
+            name: 'issue',
+            message: 'Enter the issue ID: ',
+            validate: (input) => {
+                return new Promise((resolve, reject) => {
+                    if (typeof input !== 'number' || isNaN(input)) {
+                        reject('You need to provide a number')
+                        return;
+                    }
+                    resolve(true)
+                })
+            }
+        });
+    }
+
+    const answers = await inquirer.prompt(questions);
+    return {
+        ...options,
+        type: options.type || answers.type,
+        issue: options.issue || answers.issue,
+        title: options.title || answers.title
+    };
+}
+
+export const generateEntry = async (options) => {
+    options = await promptForMissingOptions(options);
+
+    const entryData = YAML.stringify({
+        title: options.title,
+        author: '',
+        issue: options.issue,
+        type: options.type
+    })
+    const pathToEntries = path.join(process.cwd(), 'changelogs', 'unreleased')
+
+    // Test the entry by only logging it to the console if in dry-run mode
+    if (options.dryRun) {
+        console.log(x)
+        return
+    }
+
+    // Make sure the unreleased folder exists
+    if (!fs.existsSync(pathToEntries)) {
+        console.log('Changelogs directory does not exist. Creating directory ...')
+        fs.mkdirSync(pathToEntries, { recursive: true })
+        console.log('Changelogs directory created successfully')
+    }
+
+    const pathToEntry = path.join(pathToEntries, `RM-${options.issue}.yml`)
+
+    // Check if the entry already exists in the unreleased folder
+    if (fs.existsSync(pathToEntry)) {
+        if (!options.force) {
+            console.error(`The following entry already exists ${pathToEntry}`)
+            console.log('Use --force to replace it')
+            return
+        }
+    }
+
+    fs.writeFileSync(pathToEntry, entryData)
+}
